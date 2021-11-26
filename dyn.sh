c6ea84df2f242a0b6871202e10586b95d1f5080c
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-11-26 14:26:41 -0600

    Update dyn.sh

--- dyn.sh
+++ dyn.sh
@@ -3,6 +3,7 @@ host=myhome.mydomain.net.com # Your domain name on Google
 user=jIjfYhhhtY873blah # View dynamic DNS credentials on Google!
 pass=JeNnY8675309-8484 # View dynamic DNS credentials on Google!
 ipfile=mydomain.net.com/ip.php # Your ip.php file, if you want to use it
+email=myemail@gmail.com
 
 timestamp=$( date +%Y-%m-%d_%H:%M )
 old=$(nslookup $host |  awk '/Name:/{val=$NF;flag=1;next} /Address:/ && flag{print $NF}') # asks locally what your IP is for the Dynamic DNS
@@ -14,5 +15,5 @@ if [ "$old" = "$new" ]; then
    else
    echo "IP changed at $timestamp from  $old to $new" >> ~/scripts/IPs.txt
    wget -O dyn.txt -q https://$user:$pass@domains.google.com/nic/update?hostname=$host
-   echo "The IP address has changed on `date +%Y-%m-%d` at `date +%H:%M` from $old to $new" | mail -s "The IP on `$
+   echo "The IP address has changed on `date +%Y-%m-%d` at `date +%H:%M` from $old to $new" | mail -s "The IP on `$HOSTNAME` has changed" $email
 fi
