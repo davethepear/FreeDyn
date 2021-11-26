df882828d5c2537ca4aa96bb63dcd706c6dcd55a
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-11-26 14:17:58 -0600

    Update dyn.sh

--- dyn.sh
+++ dyn.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-domain=myhome.mydomain.net.com # Your domain name on Google
+host=myhome.mydomain.net.com # Your domain name on Google
+user=jIjfYhhhtY873blah # View dynamic DNS credentials on Google!
+pass=JeNnY8675309-8484 # View dynamic DNS credentials on Google!
+ipfile=mydomain.net.com/ip.php # Your ip.php file, if you want to use it
 
-nc -z ns1.google.com 53 > /dev/null
-if [ "$?" = "1" ]; then 
-   echo "It's dead, Jim!"
+timestamp=$( date +%Y-%m-%d_%H:%M )
+old=$(nslookup $host |  awk '/Name:/{val=$NF;flag=1;next} /Address:/ && flag{print $NF}') # asks locally what your IP is for the Dynamic DNS
+new=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}') # Asks Google for your IP - only use ONE, comment others out!
+# new=$(dig +short myip.opendns.com @resolver1.opendns.com) # Asks OpenDNS for your IP - only use ONE, comment others out!
+# new=$(wget -qO- $ipfile) # Asks your site for your IP - only use ONE, comment others out!
+if [ "$old" = "$new" ]; then
+   echo "IP is unchanged... this is for testing" >>/dev/null 2>&1
    else
-      timestamp=$( date +%Y-%m-%d_%H:%M )
-      # old=$(nslookup $domain |  awk '/Name:/{val=$NF;flag=1;next} /Address:/ && flag{print $NF}')
-      old=`cat /home/pi/scripts/ip.txt`
-      new=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
-      # new=$(wget -qO- http://www.linncountykansas.com/ip.php)
-      # new=$(dig +short myip.opendns.com @resolver1.opendns.com)
-      if [ "$old" = "$new" ]; then
-         echo "IP is unchanged... this is for testing" >>/dev/null 2>&1
-      else
-         echo "IP changed at $timestamp from  $old to $new" >> ~/scripts/IPs.txt
-         echo $new > /home/pi/scripts/ip.txt
+   echo "IP changed at $timestamp from  $old to $new" >> ~/scripts/IPs.txt
+   wget -O dyn.txt -q https://$user:$pass@domains.google.com/nic/update?hostname=$host
+   echo "The IP address has changed on `date +%Y-%m-%d` at `date +%H:%M` from $old to $new" | mail -s "The IP on `$
+fi
