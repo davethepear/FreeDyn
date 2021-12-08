77fb65161efddc8ded8518438d890025800e9096
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-12-07 18:41:59 -0600

    Update dyn.sh

--- dyn.sh
+++ dyn.sh
@@ -3,7 +3,7 @@ host=myhome.mydomain.net.com # Your domain name on Google
 user=jIjfYhhhtY873blah # View dynamic DNS credentials on Google!
 pass=JeNnY8675309-8484 # View dynamic DNS credentials on Google!
 ipfile=mydomain.net.com/ip.php # Your ip.php file, if you want to use it
-email=myemail@gmail.com
+email=myemail@gmail.com # email address to send updates to
 old=$(nslookup $host |  awk '/Name:/{val=$NF;flag=1;next} /Address:/ && flag{print $NF}') 
 if [ ! -z "$old" ]; then
    echo "$old" > ~/.oldip.txt
@@ -21,7 +21,7 @@ if [ ! -n "$new" ]; then
   new=$(dig +short myip.opendns.com @resolver1.opendns.com)
 fi
 if [ ! -n "$new" ]; then
-  echo "Unable to get current IP, possibly offline"
+#  echo "Unable to get current IP, possibly offline"
   exit 1 
 fi
 if [ "$old" = "$new" ]; then
