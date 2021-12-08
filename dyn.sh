3e88a7170ef43618b07486f4d80a70bd635c07a1
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-12-07 18:35:36 -0600

    Update dyn.sh

--- dyn.sh
+++ dyn.sh
@@ -4,22 +4,28 @@ user=jIjfYhhhtY873blah # View dynamic DNS credentials on Google!
 pass=JeNnY8675309-8484 # View dynamic DNS credentials on Google!
 ipfile=mydomain.net.com/ip.php # Your ip.php file, if you want to use it
 email=myemail@gmail.com
-
-timestamp=$( date +%Y-%m-%d_%H:%M )
-old=$(nslookup $host |  awk '/Name:/{val=$NF;flag=1;next} /Address:/ && flag{print $NF}') # asks locally what your IP is for the Dynamic DNS
+old=$(nslookup $host |  awk '/Name:/{val=$NF;flag=1;next} /Address:/ && flag{print $NF}') 
 if [ ! -z "$old" ]; then
    echo "$old" > ~/.oldip.txt
-   else
+   else  
    old=`cat ~/.oldip.txt`
 fi
 new=$(wget -qO- $ipfile)
-if [ -z "new" ]; then echo "Asking icanhazip for your IP" | new=$(wget -q -O - http://icanhazip.com/) ; fi
-if [ -z "new" ]; then echo "Asking google for your IP" | new=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}') ; fi
-if [ -z "new" ]; then echo "Asking opendns for your IP" | new=$(dig +short myip.opendns.com @resolver1.opendns.com) ; fi
-if [ -z "new" ]; then echo "Unable to get current IP, possibly offline" | exit 1 ; fi
-
+if [ ! -n "$new" ]; then
+  new=$(wget -q -O - http://icanhazip.com/)
+fi
+if [ ! -n "$new" ]; then
+  new=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
+fi
+if [ ! -n "$new" ]; then
+  new=$(dig +short myip.opendns.com @resolver1.opendns.com)
+fi
+if [ ! -n "$new" ]; then
+  echo "Unable to get current IP, possibly offline"
+  exit 1 
+fi
 if [ "$old" = "$new" ]; then
-   echo "IP is unchanged... this is for testing" >>/dev/null 2>&1
+   exit 0
    else
    wget -O dyn.txt -q https://$user:$pass@domains.google.com/nic/update?hostname=$host
    echo "The IP address has changed on `date +%Y-%m-%d` at `date +%H:%M` from $old to $new" | mail -s "The IP on `$HOSTNAME` has changed" $email
